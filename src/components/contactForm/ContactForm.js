d8a4dc267783a05898b300c1e2ec0ce9a584d807
Chris Mastris <cmastris@hotmail.com>
2023-07-09 14:40:34 +0100

    Implement ContactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,39 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        required
+        >
+      </input>
+      <label htmlFor="phone">Phone Number (Without Spaces)</label>
+      <input
+        id="phone"
+        type="tel"
+        name="phone"
+        value={phone}
+        onChange={e => setPhone(e.target.value)}
+        pattern="^(?:\+44|0)[1-9]\d{8,9}$"  // UK numbers starting with '+44' or '0', without spaces/separators
+        required
+        >
+      </input>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        required
+        >
+      </input>
+      <input type="submit"></input>
+    </form>
   );
 };
-
