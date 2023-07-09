5bf7d83907d7e2b661aed20db80b79b67c2eb429
Chris Mastris <cmastris@hotmail.com>
2023-07-09 13:43:22 +0100

    Pass props to ContactForm

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -28,7 +28,16 @@ export const ContactsPage = ({ contacts, onAdd }) => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
