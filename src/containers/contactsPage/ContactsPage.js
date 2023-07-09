8eeefd86f7bdd2c1f9b90abe5d5cdce0f061eb05
Chris Mastris <cmastris@hotmail.com>
2023-07-09 13:52:50 +0100

    Render TileList within ContactsPage

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -42,6 +42,7 @@ export const ContactsPage = ({ contacts, onAdd }) => {
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
