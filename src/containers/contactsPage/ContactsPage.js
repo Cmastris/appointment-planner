eaae95034cd51639e0ab84fc5ccda6fac7649486
Chris Mastris <cmastris@hotmail.com>
2023-07-09 12:19:30 +0100

    Configure name, phone, and email state

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -4,10 +4,10 @@ import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
 export const ContactsPage = ({ contacts, onAdd }) => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
